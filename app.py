f34bfdde8f3fc4a7f0f937333e5239d961516e6e
nboyer-ideo <nboyer@ideo.com>
2025-04-06 21:12:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1241,19 +1241,19 @@ def handle_leaderboard_command(ack, body, client):
         else:
             return "THE PARCHED"
     
-    header = "+=======================[ HONOR BOARD ]========================+"
-    title =  "| RANK | NAME                   | KARMA | TITLE                |"
-    divider = "|------|------------------------|--------|----------------------|"
-    footer = "+=============================================================+"
-    commands = "| /ORDER /KARMA /LEADERBOARD /REDEEM || TOP UP OR DROP OUT.   |"
+    header = "+========================[ HONOR BOARD ]=========================+"
+    title =  "| RANK  |        NAME         | KARMA |         TITLE          |"
+    divider = "|-------|---------------------|-------|-------------------------|"
+    footer = "+===============================================================+"
+    commands = "|   /ORDER  /KARMA  /LEADERBOARD  /REDEEM  || TOP UP OR DROP OUT.   |"
     
     lines = [header, title, divider]
     for i, row in enumerate(leaderboard, start=1):
-        rank = str(i).rjust(4)
-        name = row['Name'].upper()[:22].ljust(22)
+        rank = f"{i}".center(6)
+        name = row['Name'].upper()[:21].ljust(21)
         karma = f"{row['Karma']}".center(6)
-        title_str = get_title(row['Karma'])[:22].ljust(22)
-        lines.append(f"| {rank} | {name} | {karma} | {title_str} |")
+        title_str = get_title(row['Karma'])[:25].ljust(25)
+        lines.append(f"|  {rank} | {name} | {karma} | {title_str} |")
     lines.append(footer)
     lines.append(commands)
     lines.append(footer)
