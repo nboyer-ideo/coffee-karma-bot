adf4ca590fdbbfb9fc7d19b0747fc3f6a6a3d1a9
nboyer-ideo <nboyer@ideo.com>
2025-04-09 16:07:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1514,5 +1514,5 @@ def handle_cancel_order(ack, body, client):
         ]
     )
 
-if __name__ == "__main__" or True:  # `or True` forces it to run on Render too
-    flask_app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    flask_app.run(host="0.0.0.0", port=10000)
